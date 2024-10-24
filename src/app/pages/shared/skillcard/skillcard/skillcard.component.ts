d7267619bc396620862a09937bf8fdd2f957e8b1
alcamara <alhousseinec@gmail.com>
2024-10-24 18:13:46 -0500

    add skillcard component

---
+++ src/app/pages/shared/skillcard/skillcard/skillcard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skillcard',
+  standalone: true,
+  imports: [],
+  templateUrl: './skillcard.component.html',
+  styleUrl: './skillcard.component.css'
+})
+export class SkillcardComponent {
+
+}
