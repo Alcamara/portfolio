174dd892e133e2de1c1eb8c7f79f4134578d98f1
alcamara <alhousseinec@gmail.com>
2024-10-24 18:15:08 -0500

    remove test fail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './pages/shared/navbar/navbar.component';
+import { SkillcardComponent } from './pages/shared/skillcard/skillcard/skillcard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, SkillcardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
